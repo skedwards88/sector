a1fc99cee557a86f8add55c6ad0e9053eeef358f
skedwards88 <skedwards88@github.com>
2024-02-08 08:23:51 -0800

    linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,31 @@
 module.exports = {
   env: {
-    jest: true,
     node: true,
     browser: true,
     es2021: true,
     "shared-node-browser": true,
+    jest: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  overrides: [],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: "latest",
     sourceType: "module",
   },
   plugins: ["react"],
   rules: {
     "react/prop-types": "off",
+  },
+  ignorePatterns: ["dist/*"],
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
 };
