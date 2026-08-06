116272b43c24db57b9fa42e9bd74f6ee52da4548
Sarah Edwards <skedwards88@gmail.com>
2026-08-06 13:45:25 -0700

    add analytics to board game buttons

--- src/boardGameIndex.js
+++ src/boardGameIndex.js
@@ -3,7 +3,14 @@ import React from "react";
 import BoardGame from "./components/BoardGame";
 import "./App.css";
 import "./BoardGame.css";
+import {MetadataContextProvider} from "@skedwards88/shared-components/src/components/MetadataContextProvider";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<BoardGame />);
+root.render(
+  <React.StrictMode>
+    <MetadataContextProvider>
+      <BoardGame />
+    </MetadataContextProvider>
+  </React.StrictMode>,
+);
--- src/components/BoardGame.tsx
+++ src/components/BoardGame.tsx
@@ -1,29 +1,72 @@
+import {sendAnalyticsCF} from "@skedwards88/shared-components/src/logic/sendAnalyticsCF";
+import {useMetadataContext} from "@skedwards88/shared-components/src/components/MetadataContextProvider";
+
 function BoardGameButton({
   text,
   link,
+  userId,
+  sessionId,
+  analyticsDescriptor,
 }: {
   text: string;
   link: string;
+  userId: string;
+  sessionId: string;
+  analyticsDescriptor: string;
 }): React.JSX.Element {
   return (
-    <a className="boardGameButton" href={link} aria-label={text}>
+    <a
+      className="boardGameButton"
+      href={link}
+      aria-label={text}
+      onClick={() => {
+        sendAnalyticsCF({
+          userId,
+          sessionId,
+          analyticsToLog: [
+            {eventName: "boardGame", eventInfo: {click: analyticsDescriptor}},
+          ],
+        });
+      }}
+    >
       {text}
     </a>
   );
 }
+
 export default function BoardGame(): React.JSX.Element {
+  const {userId, sessionId} = useMetadataContext();
+
   return (
     <div className="boardGame">
       <h1>Sector Board Game</h1>
-      <BoardGameButton link="/boardGameRules.pdf" text="Detailed rules (PDF)" />
-      <BoardGameButton link="/" text="Play online" />
+      <BoardGameButton
+        link="/boardGameRules.pdf"
+        text="Detailed rules (PDF)"
+        userId={userId}
+        sessionId={sessionId}
+        analyticsDescriptor="rules"
+      />
+      <BoardGameButton
+        link="/"
+        text="Play online"
+        userId={userId}
+        sessionId={sessionId}
+        analyticsDescriptor="onlinePlay"
+      />
       <BoardGameButton
         link="https://www.thegamecrafter.com/"
         text="Buy the game (Coming soon)"
+        userId={userId}
+        sessionId={sessionId}
+        analyticsDescriptor="buy"
       />
       <BoardGameButton
         link="https://twistedtrailgames.com/"
         text="Explore other games"
+        userId={userId}
+        sessionId={sessionId}
+        analyticsDescriptor="moreGames"
       />
     </div>
   );
